43f9c769e59b7416c5601b2819ae566e988e5c1b
ikr7 <ikura.gm@gmail.com>
2013-08-21 21:31:25 +0900

    Update login.php

--- example/login.php
+++ example/login.php
@@ -3,15 +3,17 @@
 //ライブラリ読み込み
 require_once("../croudia4php.php");
 
+//セッション開始
 session_start();
 
-// Facebookのキー
-$ci = "4d779a25016826d604c7492310719e02936e1d89e420247a0473370a4a733886";
-$cs = "54fb4521c543e2511e166e3a46175d0f20503c7b4150af56b7e5c5243e4decce";
+// コンシューマキー
+$ci = "YOUR_CLIENT_KEY";
+$cs = "YOUR_CLIENT_SECRET";
 
 $_SESSION["c4p"] = new CroudiaForPHP($ci, $cs);
 $c4p = $_SESSION["c4p"];
 
+//認証用URLをリンクとして表示する
 $url = $c4p -> getAuthorizeURL();
 
 echo "<a href=\"".$url."\">LOGIN</a>";
